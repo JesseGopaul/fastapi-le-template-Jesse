c767c1cb36cbe3f0d2678c48a6b70881788c99c8
JesseGopaul <jessegopaul.sr@gmail.com>
2026-04-10 12:31:35 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
 from .models import *
 from .main import *
 https://claude.ai/chat/7c3c0234-2141-455c-ac72-c9ed56e734c9
+https://gemini.google.com/app/b7157e8149ec629e?utm_source=app_launcher&utm_medium=owned&utm_campaign=base_all
